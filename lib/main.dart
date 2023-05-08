62a1431b0f74af5f73ba98bb02d2f17971bd9f26
Shivam Bajaj <bajjuoo7s@gmail.com>
2023-05-09 00:56:34 +0530

    uuid added and enum category to expenses model

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:xpenses/widgets/new_transaction.dart';
-import 'package:xpenses/widgets/transaction_list.dart';
+import 'package:xpenses/widgets/expenses_list.dart';
 
-import 'models/transaction.dart';
+import 'models/expenses.dart';
 
 void main() => runApp(MyApp());
 
@@ -25,50 +24,71 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-   final List<Transaction> _userTransactions=[
-    Transaction(id: 't1', title: 'cigg', amount: 330, date: DateTime.now()),
-    // Transaction(id: 't2', title: 'cigg1', amount: 300, date: DateTime.now()),
-    // Transaction(id: 't3', title: 'cigg2', amount: 350, date: DateTime.now()),
-    // Transaction(id: 't3', title: 'cigg2', amount: 350, date: DateTime.now()),
-    // Transaction(id: 't3', title: 'cigg2', amount: 350, date: DateTime.now()),
-    // Transaction(id: 't3', title: 'cigg2', amount: 350, date: DateTime.now()),
-    // Transaction(id: 't3', title: 'cigg2', amount: 350, date: DateTime.now()),
-    Transaction(id: 't3', title: 'cigg2', amount: 350, date: DateTime.now()),
+  final List<Expense> _userExpenses = [
+    Expense(
+        title: 'cig1',
+        amount: 330,
+        date: DateTime.now(),
+        category: Category.food),
+    Expense(
+      title: 'cig2',
+      amount: 334,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+        title: 'c4',
+        amount: 340,
+        date: DateTime.now(),
+        category: Category.leisure),
+    Expense(
+        title: 'c5',
+        amount: 350,
+        date: DateTime.now(),
+        category: Category.food),
   ];
 
-  void _addNewTransaction(String title, int amount){
-    final newTx = Transaction(id: DateTime.now().toString(), title: title, amount: amount, date: DateTime.now());
+  void _addNewTransaction(String title, int amount) {
+    final newTx = Expense(
+        id: DateTime.now().toString(),
+        title: title,
+        amount: amount,
+        date: DateTime.now());
     setState(() {
-      _userTransactions.add(newTx);
+      _userExpenses.add(newTx);
     });
   }
 
-  void _startAddNewTransaction(BuildContext ctx){
-
-    showModalBottomSheet(context: ctx, builder:((_) {
-      return NewTransaction(_addNewTransaction);
-    }
-    ));
-
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: ((_) {
+          return NewTransaction(_addNewTransaction);
+        }));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Xpenses'),
+        title: const Text('Xpenses'),
         centerTitle: true,
         elevation: 10,
         backgroundColor: Colors.deepPurpleAccent,
-        shape: RoundedRectangleBorder(
+        shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.vertical(bottom: Radius.circular(20))),
         toolbarHeight: 70,
         actions: [
-          IconButton(icon: Icon(Icons.add_outlined), onPressed:(() => _startAddNewTransaction(context))),
+          IconButton(
+              icon: const Icon(Icons.add_outlined),
+              onPressed: (() => _startAddNewTransaction(context))),
         ],
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
-      floatingActionButton: FloatingActionButton(child: Icon(Icons.add),onPressed:(() => _startAddNewTransaction(context)),),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: (() => _startAddNewTransaction(context)),
+      ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -82,7 +102,11 @@ class _HomePageState extends State<HomePage> {
                   color: Colors.blue,
                   elevation: 7,
                 )),
-            TransactionList(_userTransactions),
+            Expanded(
+              child: ExpensesList(
+                expenses: _userExpenses,
+              ),
+            ),
           ],
         ),
       ),
--- lib/models/transaction.dart
+++
@@ -1,9 +0,0 @@
-class Transaction{
-  final String id;
-  final String title;
-  final int amount;
-  final DateTime date;
-
-  Transaction({required this.id, required this.title, required this.amount, required this.date});
-
-}
